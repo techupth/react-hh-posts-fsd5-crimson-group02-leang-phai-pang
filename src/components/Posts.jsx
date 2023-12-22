fef947ee6f528c32fa6e799b03e07a22523e88ea
pang <pangton28.p@gmail.com>
2023-12-22 16:00:16 +0700

    feat(exercise): add complete Manipulating Array State

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,29 +1,59 @@
+import { useState } from "react";
+import postData from "../data/Post";
+
 function Posts() {
+  const [listPost, setListPost] = useState([...postData]);
+
+  const handleAddLike = (e, index) => {
+    e.preventDefault();
+    const newListPost = [...listPost];
+    newListPost[index].likes += 1;
+    setListPost(newListPost);
+  };
+
+  const handleDisLike = (e, index) => {
+    e.preventDefault();
+    const newListPost = [...listPost];
+    newListPost[index].likes -= 1;
+    if (newListPost[index].likes < 0) {
+      newListPost[index].likes = 0;
+    }
+    setListPost(newListPost);
+  };
+
   return (
     <div class="app-wrapper">
       <h1 class="app-title">Posts</h1>
-      <div class="post-list">
-        <div class="post-item">
-          <div class="post-header">
-            <h2>Post Title #1</h2>
-            <div class="post-social-media-stats">
-              <span class="stats-topic">Likes: </span>
-              <span class="post-likes">10</span>
+      {listPost.map((item, index) => (
+        <div class="post-list" key={index}>
+          <div class="post-item">
+            <div class="post-header">
+              <h2>
+                {item.title} #{item.id}
+              </h2>
+              <div class="post-social-media-stats">
+                <span class="stats-topic">Likes: </span>
+                <span class="post-likes">{item.likes}</span>
+              </div>
+            </div>
+            <p class="post-content">{item.content}</p>
+            <div class="post-actions">
+              <button
+                class="like-button"
+                onClick={(e) => handleAddLike(e, index)}
+              >
+                Like
+              </button>
+              <button
+                class="dislike-button"
+                onClick={(e) => handleDisLike(e, index)}
+              >
+                Dislike
+              </button>
             </div>
           </div>
-          <p class="post-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
-            tincidunt, leo non molestie consectetur, elit libero faucibus
-            tellus, sed fringilla tortor libero sit amet odio. Maecenas sed ante
-            condimentum mauris euismod pellentesque eu eu justo...
-          </p>
-          <div class="post-actions">
-            <button class="like-button">Like</button>
-            <button class="dislike-button">Dislike</button>
-          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
